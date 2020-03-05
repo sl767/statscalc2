1288b5b5ee72cd5892611f29a2670e782f48c9fc
stacy <sl767@njit.edu>
2020-03-05 16:22:44 -0500

    added Statistical functions

---
+++ Statistics/Variance.py
@@ -0,0 +1,7 @@
+import numpy
+
+
+class Variance:
+    @staticmethod
+    def variance(data):
+        return numpy.var(data)
---
+++ Statistics/Zscore.py
@@ -0,0 +1,11 @@
+from RandomNumGenerator.SelectSeed import PickSeed
+from Statistics.StandardDeviation import StandardDeviation
+from Statistics.Mean import Mean
+
+class Z_score():
+    @staticmethod
+    def z_score(theSeed, data):
+        X = PickSeed.pickSeed(theSeed, data)
+        mean = Mean.mean(data)
+        stdDev = StandardDeviation.standardDeviation(data)
+        return (X-mean)/stdDev
