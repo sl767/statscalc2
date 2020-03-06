00046724863f471a6d012e3467dda63debe5e2a8
stacy <sl767@njit.edu>
2020-03-06 15:42:03 -0500

    added functions

--- Statistics/Zscore.py
+++ Statistics/Zscore.py
@@ -2,10 +2,11 @@ from RandomNumGenerator.SelectSeed import PickSeed
 from Statistics.StandardDeviation import StandardDeviation
 from Statistics.Mean import Mean
 
+
 class Z_score:
     @staticmethod
     def z_score(theSeed, data):
         X = PickSeed.pickSeed(theSeed, data)
         mean = Mean.mean(data)
         stdDev = StandardDeviation.standardDeviation(data)
-        return (X-mean)/stdDev
+        return (X - mean) / stdDev
