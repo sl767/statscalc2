370d88caf3f4ff6f1d08ebe2240f6df75d2969c1
stacy <sl767@njit.edu>
2020-03-07 19:52:51 -0500

    updated syntax

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -1,9 +1,9 @@
 from Calculator.Calculator import Calculator
-from Statistics.Mean import mean
+from Statistics.Mean import Mean
 
 
 class Statistics(Calculator):
 
     def mean(self, data):
-        self.result = mean(data)
+        self.result = Mean(data)
         return self.result
